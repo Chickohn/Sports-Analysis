81960da5dc0b630b85c2c651689dde17befe1591
Freddie Kohn <frederick.kohnny@gmail.com>
2025-06-04 21:00:41 +0100

    Add project files.

---
+++ Sports Analysis/Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+namespace Sports_Analysis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+//im a goofy goober
